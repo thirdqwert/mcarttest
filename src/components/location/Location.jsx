cd3f8ba5afd6b1fca404b87c39cffe4655dd037c
qwert <qwert@gmail.com>
2025-08-10 07:24:48 +0500

    seven

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -2,7 +2,7 @@ import s from './Location.module.scss'
 const Location = () => {
     return (
         <>
-            <div className={s.location}>
+            <div className={s.location} data-aos='fade-up'>
                 <div className={s.texts}>
                     <p>С нетерпением ждём вас к 18:00, чтобы вместе начать этот особенный</p>
                     <span>для нас вечер</span>
@@ -10,7 +10,6 @@ const Location = () => {
                     <span>улица Каранкол, 88</span>
                 </div>
                 <a href="https://yandex.ru/maps/org/16569065793">открыть карту</a>
-
             </div>
         </>
     )
