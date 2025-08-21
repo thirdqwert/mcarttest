97135f14552a225fffffc961f399ba4f297e99e4
qwert <qwert@gmail.com>
2025-08-21 15:24:56 +0500

    fixed textslide

--- src/components/textSlide/TextSlide.jsx
+++ src/components/textSlide/TextSlide.jsx
@@ -8,10 +8,10 @@ const TextSlide = () => {
             <div>
                 <div className={s.texts}>
                     <Marquee direction="right" autoFill={true} speed={30} className={s.text}>
-                        <p className={s.text}>I love you Ti amo Ich liebe dich Je t’aime</p>
+                        <p className={s.text}>I love you - Ti amo - Ich liebe dich - Je t’aime </p>
                     </Marquee>
                     <Marquee direction="right" autoFill={true} speed={30} className={s.text}>
-                        <p className={s.text}>Men seni sevaman Я тебя люблю Te quiero</p>
+                        <p className={s.text}>Men seni sevaman - Я тебя люблю - Te quiero</p>
                     </Marquee>
                 </div>
             </div>
